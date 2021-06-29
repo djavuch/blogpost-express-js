321151f0506a334aba8581b4ce1d2b0f64d30bb2
Krosent <krosent2@gmail.com>
2021-06-29 23:29:23 +0200

    replace update with updateOne

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.post('/articles/edit/:id', (req, res) => {
 
     let query = { _id: req.params.id }
 
-    Article.update(query, article, function (err) {
+    Article.updateOne(query, article, function (err) {
         if (err) {
             console.log(err)
             return
